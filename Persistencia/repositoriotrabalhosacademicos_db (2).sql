f900ab402ad241e160b7ef81784d0d8ce211fad8
Alan A. Correa <alan.correa.sul@gmail.com>
2013-08-15 14:59:27 -0300

    Estrutura banco de dados alterada

--- Persistencia/repositoriotrabalhosacademicos_db (2).sql
+++ Persistencia/repositoriotrabalhosacademicos_db (2).sql
@@ -129,16 +129,14 @@ CREATE TABLE IF NOT EXISTS `autor` (
   `email` varchar(150) DEFAULT NULL,
   `observacao` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
 
 --
 -- Extraindo dados da tabela `autor`
 --
 
 INSERT INTO `autor` (`id`, `nome`, `email`, `observacao`) VALUES
-(1, 'Thiago', 'contato@thiagobelem.net', 'obs'),
-(2, 'Fulano da Silva', 'fulano@email.com', 'obs'),
-(3, 'Ciclano', 'ciclano@uol.com.br', 'obs');
+(32, 'Alan Alves Correa', 'alan.correa.sul@gmail.com', 'Fone: 05199880070');
 
 -- --------------------------------------------------------
 
@@ -152,15 +150,14 @@ CREATE TABLE IF NOT EXISTS `instituicao` (
   `email` varchar(150) DEFAULT NULL,
   `observacao` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Extraindo dados da tabela `instituicao`
 --
 
 INSERT INTO `instituicao` (`id`, `nome`, `email`, `observacao`) VALUES
-(1, 'Faculdade Cenecista de Osório', 'facos@mail.com', 'fone:3663-0852'),
-(2, 'Instituto Federal - campo Osório', 'mail@mail.com', 'fone:3663-8800');
+(4, 'FACULDADE CENECISTA DE OSÃ“RIO', 'facos@facos.edu.br', 'CNEC/OsÃ³rio\r\nFACOS - Faculdade Cenecista de OsÃ³rio\r\nInstituto de EducaÃ§Ã£o Cenecista MarquÃªs de Herval\r\n\r\nRua 24 de Maio, 141, Centro - OsÃ³rio/RS\r\nFone: (51) 2161.0200                                                ');
 
 -- --------------------------------------------------------
 
@@ -174,15 +171,14 @@ CREATE TABLE IF NOT EXISTS `orientador` (
   `email` varchar(150) DEFAULT NULL,
   `observacao` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Extraindo dados da tabela `orientador`
 --
 
 INSERT INTO `orientador` (`id`, `nome`, `email`, `observacao`) VALUES
-(1, 'Andrio Pinto dos Santos', 'mail@mail.com', 'mestre em computação aplicada'),
-(2, 'Renata Zanella', 'renata@mail.com', 'reside em Porto Alegre');
+(5, 'Mariusa Warpechowski', 'mariusaw@gmail.com', 'Professora da disciplina de Tecnologia da informaÃ§Ã£o e produÃ§Ã£o e metodologia de material educativo                                         ');
 
 -- --------------------------------------------------------
 
@@ -192,7 +188,7 @@ INSERT INTO `orientador` (`id`, `nome`, `email`, `observacao`) VALUES
 
 CREATE TABLE IF NOT EXISTS `trabalho_academico` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `titulo` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `tipo` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `resumo` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `abstract` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
@@ -205,19 +201,14 @@ CREATE TABLE IF NOT EXISTS `trabalho_academico` (
   `instituicao` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `orientador` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
 
 --
 -- Extraindo dados da tabela `trabalho_academico`
 --
 
 INSERT INTO `trabalho_academico` (`id`, `titulo`, `tipo`, `resumo`, `abstract`, `numero_paginas`, `data`, `local_pdf`, `palavras_chave`, `area`, `aluno`, `instituicao`, `orientador`) VALUES
-(22, 'a', NULL, 'a', 'a', 1, '0000-00-00 00:00:00', 'http://www.freewebmasterhelp.com/tutorials/phpmysql/4', 'tecnologia digital', 'CIÃŠNCIA DA COMPUTAÃ‡ÃƒO', '', '', ''),
-(23, 's', NULL, 's', 's', 56, '0000-00-00 00:00:00', 'http://www.freewebmasterhelp.com/tutorials/phpmysql/4', 'tecnologia digital', 'GEOCIÃŠNCIAS', '', '', ''),
-(24, 'o', NULL, 'o', 'o', 12, '0000-00-00 00:00:00', 'http://www.freewebmasterhelp.com/tutorials/phpmysql/4', 'tecnologia digital', 'PROBABILIDADE E ESTATÃSTICA', '', '', ''),
-(25, 'novo', NULL, 'wwww', 'wwww', 23, '0000-00-00 00:00:00', 'C:UsersPublicPicturesSample Picturesa.pdf', 'tecnologia digital', 'ARQUEOLOGIA', '', '', ''),
-(26, 'agora Ã© pra valer', NULL, 'resumo agora Ã© pra valer', 'abstract agora Ã© para valer', 89, '2013-04-04 11:04:23', 'http://informatica.besaba.com/conteudo/xdk%20intel.txt', 'tecnologia digital', 'AGRONOMIA', 'Ciclano', '', ''),
-(27, 'novo', NULL, 'a', 'a', 23, '0000-00-00 00:00:00', 'eeee', 'tecnologia digital', 'ASTRONOMIA', 'Fulano da Silva', 'Instituto Federal - campo Osï¿½rio', 'Renata Zanella');
+(33, 'USO DOS MOTORES DE BUSCA NA INTERNET: como se configuram as pesquisas de conteÃºdo na Web para a produÃ§Ã£o de trabalhos educacionais', 'Monografia', 'Este trabalho de pesquisa busca entender e demonstrar como se configura a pesquisa de\r\nconteÃºdo na Internet, a partir dos  search enginese, como se dÃ¡ o uso dos filtros de busca\r\nnesse Ã¢mbito.  A pesquisa Ã© uma prÃ¡tica comum e consolidada no contexto educacional,\r\nobjetivando  a  busca  de  insumos  adequados  para  a  produÃ§Ã£o  de  trabalhos,  apoiados\r\nbibliograficamente e, capazes de fomentar as discussÃµes intelectuais. Nesse sentido, entendese o termo pesquisa como uma derivaÃ§Ã£o do latim  perquiro, que devido a transformaÃ§Ãµes\r\nfonÃ©ticas ao longo da histÃ³ria teve o R transformado por S, dando origem ao termo pesquisa.\r\nÃ‰ vÃ¡lido ressaltar a diferenciaÃ§Ã£o das pesquisas, classificando-as em Pesquisa Escolar ou\r\nPesquisa de ConteÃºdo e Pesquisa CientÃ­fica. A Pesquisa de ConteÃºdo, deve ser desenvolvida\r\natravÃ©s de vÃ¡rias fontes bibliogrÃ¡ficas, visuais, orais entre outras, com o objetivo de subsidiar\r\na aprendizagem de determinados conceitos. Posicionado nesse contexto, este Trabalho de\r\nConclusÃ£o de Curso, visou investigar como se configura a pesquisa de conteÃºdo por parte dos\r\nalunos da turma de Tecnologia da InformaÃ§Ã£o do curso de Letras da Faculdade Cenecista de\r\nOsÃ³rio-RS, para a composiÃ§Ã£o de trabalhos textuais com o uso dos sites de buscas. Essa turma\r\nfoi acompanhada em duas aulas, tendo o histÃ³rico de navegaÃ§Ã£o do browser de Internet\r\nanalisado e questionÃ¡rios aplicados, com vistas ao levantamento de dados para futura anÃ¡lises.\r\nEste estudo demostrou o Google como escolha preferida para a realizaÃ§Ã£o de pesquisas de\r\nconteÃºdo, e os impactos que os filtros de busca produzem nos processo de pesquisa na Web.', 'This research work tries to understand and demonstrate how is configured the research of\r\ncontents in Internet, from search engines and, how the use of filters work. The research is a\r\npractice comum and consolidated in an educational context, aiming a search for appropriate\r\ninputs  for  production  of  works,  bibliographically  supported  and  capable  to  promote\r\nintelectual discussions. Accordingly, it means the term PESQUISAlike a derivation from latin\r\nword PERQUIRO, that due phonetics transformations along the years, had the R transformed\r\ninto a  S, origining  the  term  PESQUISA (research). It`s  important  the  differentiation of\r\nresearches, classifying them into scholar research, content research and cientific research.\r\nThe content research, must be developed among several bibliographic sources, visual, oral\r\nand others, with the main objective of subsidizethe learning of some concepts. Positioned on\r\nthis concept, this work on course conclusion aims to investigate how the content research is\r\nconfigured from some students of Information tecnology from Letters course of Faculdade\r\ncenecista  de  OsÃ³rio-  RS,  for  textual  composition  using  search  sites.  This  class  was\r\naccompanied in two days, having the browser navigation history from Internet analyzed and\r\nan  applied  survey,  looking  for  data  levy  to  be  used  in  future  analysis.  This  research\r\ndemonstrated  Google  as  preferred  choose  for  content  researches  and  the  impacts  that\r\nresearch filters produce on the process of web research.', 82, '2013-10-00 17:00:00', 'http://informatica.besaba.com/conteudo/USO%20DOS%20MOTORES%20DE%20BUSCA%20NA%20INTERNET%20-%20TCC%20Alan%20Alves%20Correa.pdf', 'Internet. Filtros de Busca. Pesquisa de conteÃºdo. Motores de Busca. Google.', 'EDUCAï¿½ï¿½O', 'Alan Alves Correa', 'FACULDADE CENECISTA DE OSÃ“RIO', 'Mariusa Warpechowski');
 
 -- --------------------------------------------------------
 
