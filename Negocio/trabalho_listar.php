dac0e7f1f88c35c2f56767cd4ce5330cd5f20b59
Alan A. Correa <alan.correa.sul@gmail.com>
2013-09-09 14:06:19 -0300

    Página listar trabalhos cadastrados

---
+++ Negocio/trabalho_listar.php
@@ -0,0 +1,74 @@
+<?php
+include '../negocio/seguranca.php';
+?>
+<?php
+//Conexão e consulta ao Mysql
+include ('../persistencia/classe_conexao.php');
+include 'classe_mensagens_formularios.php';
+$novaConexao = new conexao();
+$novaConexao->conecta();
+$qry = mysql_query("select titulo,aluno,orientador from trabalho_academico");
+
+
+//Pegando os nomes dos campos
+
+$num_fields = mysql_num_fields($qry); //Obtém o número de campos do resultado
+
+for ($i = 0; $i < $num_fields; $i++) {//Pega o nome dos campos
+    $fields[] = mysql_field_name($qry, $i);
+}
+
+//Montando o cabeçalho da tabela
+$table = '<table border="1"><tr>';
+
+for ($i = 0; $i < $num_fields; $i++) {
+    $table .= '<th>' . $fields[$i] . '</th>';
+}
+
+//Montando o corpo da tabela
+$table .= '<tbody>';
+while ($r = mysql_fetch_array($qry)) {
+    $table .= '<tr>';
+    for ($i = 0; $i < $num_fields; $i++) {
+        $table .= '<td>' . $r[$fields[$i]] . '</td>';
+    }
+    $table .= '</tr>';
+}
+
+//Finalizando a tabela
+$table .= '</tbody></table>';
+?>
+
+<!DOCTYPE html> 
+<html lang="en"> 
+    <head> 
+        <meta charset="utf-8"> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <link href="http://www.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"> 
+        <script src="http://www.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <?php include '../tela/header.html'; ?>
+
+        <!-- CLASSE QUE DEFINE O CONTAINER COMO FLUIDO (100%) -->
+        <div class="container-fluid">
+            <!-- CLASSE PARA DEFINIR UMA LINHA -->
+            <div class="row-fluid">
+                <!-- COLUNA OCUPANDO 2 ESPAÇOS NO GRID -->
+                <?php include '../tela/menu_esquerdo.html'; ?>
+                <!-- COLUNA OCUPANDO 10 ESPAÇOS NO GRID -->
+                <br><br><br>
+                <div class="span10">
+                    <?php echo 'Olá ' . $_SESSION['UsuarioNome'] . '!'; ?> 
+                    <?php echo "<a href='../negocio/logout.php'>Sair do sistema</a>"; ?>
+
+                    <hr />  
+                    <legend class="breadcrumb">Trabalhos acadêmicos cadastrados</legend>
+
+                    <?php echo $table; ?>
+
+                </div>
+            </div>
+            <?php include '../tela/rodape.html'; ?>
+    </body>
+</html>
--- Persistencia/classe_conexao.php
+++ Persistencia/classe_conexao.php
@@ -1,4 +1,5 @@
 <?php
+
 class conexao {
 
     var $host = 'localhost';
@@ -7,11 +8,15 @@ class conexao {
     var $banco = 'repositoriotrabalhosacademicos_db';
     var $conexao = null;
     var $query = null;
+   
 
     function conecta() {
         $this->conexao = mysql_connect($this->host, $this->usuario, $this->senha);
         $status = mysql_select_db($this->banco, $this->conexao);
         return $status;
     }
+
+
 }
+
 ?>
